28f5b7c4a41d2ffb8975012ed4fa38f1f35be502
Shovan61 <shovonmazumder61@gmail.com>
2025-10-27 23:19:34 +0100

    feat: integrate Clerk authentication with sign-in and sign-up pages, update styles, and add middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="font-bold text-4xl">WebLoom App</div>;
+  return <div className="font-bold text-4xl text-rose-900 h-screen">WebLoom App</div>;
 }
