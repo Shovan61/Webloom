221f8583c9e5c849465b15f0c9afeb8ad28cae93
Shovan61 <shovonmazumder61@gmail.com>
2025-11-07 23:19:02 +0100

    feat: rename PDomain to AgencyPage and implement user authentication check

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,7 +1,15 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function PDomain() {
+async function AgencyPage() {
+  const authUser = await currentUser();
+
+  if (!authUser) redirect("/sign-in");
+
+  // Get the User details
+
   return <div>Agency Dashboard</div>;
 }
 
-export default PDomain;
+export default AgencyPage;
